e39ce7a5ab263c8322bb50978333ed68c2fc1fbc
Luca Wellmeier <luca_wellmeier@gmx.de>
2020-09-08 22:53:42 +0200

    design

--- assets/functions.js
+++ assets/functions.js
@@ -3,12 +3,6 @@ function toggleMenu() {
 	menu.classList.toggle('menu-xs-hide');
 }
 
-function onMailClick(x) {
-	window.location.href = 'mailto:' + x.dataset.name 
-		+ '@' + x.dataset.domain + '.' + x.dataset.tld;
-	return false;
-}
-
 window.onload = function() {
 	toggleMenu();
 }
